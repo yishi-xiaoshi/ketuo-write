dc94f037bd34d0010658fc8121ca9cc13cae5987
一石小石 <3149819274@qq.com>
2026-05-06 19:19:08 +0800

    fix: 修复 GitHub Pages 部署路径，base 改为 /ketuo-write/

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: './',
+  base: '/ketuo-write/',
   plugins: [react(), tailwindcss()],
   server: {
     headers: {
